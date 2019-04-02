cb367186fba192a751da6897b50bde664b66d84d
Joel Rhine <baronontherhine@gmail.com>
2019-04-02 00:29:03 -0700

    added email form input

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './index.scss';
-import { Hero } from "react-bulma-components/full";
-import { Section } from "react-bulma-components/full";
+import { 
+  Section,
+  Heading,
+  Container,
+  Hero, } from "react-bulma-components/full";
 
 class App extends Component {
   render() {
     return (
-      <Section>
-        <Hero color="primary" gradient>
-          <Hero.Body>
-            <h1 className="title">Hello 👋</h1>
-            <h2 className="subtitle">Thanks for stopping by!</h2>
-          </Hero.Body>
-        </Hero>
-      </Section>
-      
+      <div className="App">
+        <Section>
+          <Hero color="primary" gradient>
+            <Hero.Body>
+              <h1 className="title">Hello 👋</h1>
+              <h2 className="subtitle">Thanks for stopping by!</h2>
+            </Hero.Body>
+          </Hero>
+        </Section>
+        <Content />
+      </div>      
     );
   }
 }
 
-class River extends Component {
-  constructor(props) {
-    super(props)
-  }
+class Content extends Component {
   render() {
     return (
-      <div id="test-button" className="card">
-        <div className="card-content">TEST</div>
-      </div>
+      <Section>
+        <Container>
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="email" placeholder="you@arecool.com" />
+            </div>
+            <a class="button has-background-info has-text-white">Get in touch</a>
+          </div>
+        </Container>
+      </Section>
     )
   }
 }
