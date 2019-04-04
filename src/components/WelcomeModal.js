a06e2554cdba99417fc19bf4de40d80e95cabc22
Joel Rhine <baronontherhine@gmail.com>
2019-04-04 13:28:13 -0700

    improved file structure, added component

---
+++ src/components/WelcomeModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class WelcomeModal extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
