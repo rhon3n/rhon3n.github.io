32b625f213a9e57e52347fc1395858b0270a49b4
Joel Rhine <baronontherhine@gmail.com>
2019-04-10 11:12:10 -0700

    Added helper comment

--- src/components/componentIndex.js
+++ src/components/componentIndex.js
@@ -1,3 +1,7 @@
+/* This file serves as an export module for the entire components directory. Any new components that are added must be added to this file using the "*" wildcard. Ex. a Nav.js component module would be added like this: export * from './Nav';
+*
+*/
+
 export * from './CenteredColumn';
 export * from './EmailForm';
 export * from './Tiles';
