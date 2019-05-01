b233041c9a7deb2f28e3f72eae0b8c0ed2c28e2b
Joel Rhine <baronontherhine@gmail.com>
2019-05-01 13:11:53 -0700

    added dark/light component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,6 +11,10 @@ import {
           <Icon size="large">
               <FontAwesomeIcon icon={["fas", "handshake"]} size="2x" className="is-primary has-text-centered has-text-grey-dark" />
           </Icon>
+          <div className="button has-addons is-right">
+            <span className="button">dark</span>
+            <span className="button">light</span>
+          </div>
         </Navbar>
       )
     }
