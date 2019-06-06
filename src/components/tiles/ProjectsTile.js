81dca54379aba3ddcc4d84e4ed60b337740d783b
Joel Rhine <baronontherhine@gmail.com>
2019-06-06 12:44:12 -0700

    added ProjectsTile boilerplate

---
+++ src/components/tiles/ProjectsTile.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Tile } from 'react-bulma-components/full';
+
+export class ProjectsTile extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ProjectsTile;
