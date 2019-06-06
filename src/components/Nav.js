1b72d9ee405351b461888ba80ad0e565c1cf8c30
Joel Rhine <baronontherhine@gmail.com>
2019-06-06 10:57:26 -0700

    added layout components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,17 @@ import {
   Icon,
   Navbar, } from "react-bulma-components/full";
 
-  export class Nav extends Component {
-    render() {
-      return (
-        <div className="is-block" >
-          <Navbar fixed="top">
-            <Icon size="large">
-                <FontAwesomeIcon icon={["fas", "handshake"]} size="2x" className="is-primary has-text-centered has-text-grey-dark" />
-            </Icon>
-          </Navbar>
-          
-        </div>
-      )
-    }
+export class Nav extends Component {
+  render() {
+    return (
+      <div className="is-block" >
+        <Navbar fixed="top">
+          <Icon size="large">
+              <FontAwesomeIcon icon={["fas", "handshake"]} size="2x" className="is-primary has-text-centered has-text-grey-dark" />
+          </Icon>
+        </Navbar>
+        
+      </div>
+    )
   }
+}
