31707f47daf2e78ba915c1984fca8855816c893e
Joel Rhine <baronontherhine@gmail.com>
2019-06-08 09:21:47 -0700

    Added SkillsTile

--- src/components/tiles/SkillsTile.js
+++ src/components/tiles/SkillsTile.js
@@ -3,7 +3,23 @@ import { Tile } from 'react-bulma-components/full';
 
 export class SkillsTile extends Component {
   render() {
-    return <div />;
+    return (
+      <Tile kind="child" notification>
+        <span className="title has-background-grey-dark is-size-3 has-text-white highlight-title">SKILLS</span>
+        <br />
+        <br />
+        <div className="has-text-left is-size-5">
+          <li>Node</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Heroku</li>
+          <li>HTML/CSS</li>
+          <li>Git</li>
+          <li>Vue</li>
+          <li>Responsive Web Dev</li>
+        </div>
+      </Tile>
+    );
   }
 }
 
