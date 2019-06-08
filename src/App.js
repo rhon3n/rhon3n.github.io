9df49e3face5357b4a1001115f4d423729474d12
Joel Rhine <baronontherhine@gmail.com>
2019-06-08 09:25:39 -0700

    Imported layout components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './dopestyles.scss';
 import './index.scss';
 import { Section } from "react-bulma-components/full";
 import { CenteredColumn, TileBox, RhonenFooter, ContactInfo, MainHero, Nav } from './components/componentIndex';
+import { Footer } from './components/layout/Footer';
+import { Main } from './components/layout/Main';
+import { Hero } from './components/layout/Hero';
+import { Nav } from './components/layout/Nav';
 import './helpers/fontawesome';
 
 class App extends Component {
