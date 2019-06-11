fff5f11730356379346cf7b1a22732a4c4d30fa9
Joel Rhine <baronontherhine@gmail.com>
2019-06-11 11:59:07 -0700

    Added description

--- src/components/TileBox.js
+++ src/components/TileBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Tile } from 'react-bulma-components/full';
 import { SkillsTile, ExperienceTile, GoalTile, WorkTile, EducationTile } from './componentIndex';
 
+// This is a layout composition component for any components that are bulma tiles.
 export class TileBox extends Component {
   render() {
     return (
