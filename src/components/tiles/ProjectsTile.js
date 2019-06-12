51f7440c2a5bb889872a74265193577eda7bcb8c
Joel Rhine <baronontherhine@gmail.com>
2019-06-12 10:44:36 -0700

    changed variable name

--- src/components/tiles/ProjectsTile.js
+++ src/components/tiles/ProjectsTile.js
@@ -7,19 +7,19 @@ export class ProjectsTile extends Component {
   state = {
     title: 'RECENT PROJECTS',
     subtitle: 'React Todo App',
-    todoUrl: 'https://github.com/rhon3n/react-todo-list',
+    todoURL: 'https://github.com/rhon3n/react-todo-list',
     body:
       'A simple react application that gets a list of dummy todos from JSONplaceholder. This app allows you to add or remove todos from the list. Since this app is used to demonstrate my ability to control state among components in React without using Redux, it does not have a working backend—any changes made will not persist after a page reload.',
   };
 
   render() {
-    const { title, body, subtitle, todoUrl } = this.state;
+    const { title, body, subtitle, todoURL } = this.state;
     return (
       <Tile kind="parent" size={12}>
         <Tile kind="child" notification>
           {' '}
           <TileTitle title={title} />
-          <TileBody url={todoUrl} title={subtitle} body={body} />
+          <TileBody url={todoURL} title={subtitle} body={body} />
         </Tile>
       </Tile>
     );
