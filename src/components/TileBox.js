68f88604625794053fced8326bfccd79cf061881
Joel Rhine <baronontherhine@gmail.com>
2019-06-11 22:26:52 -0700

    added projects tile

--- src/components/TileBox.js
+++ src/components/TileBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Tile } from 'react-bulma-components/full';
-import { SkillsTile, ExperienceTile, GoalTile, WorkTile, EducationTile } from './componentIndex';
+import { SkillsTile, ExperienceTile, GoalTile, WorkTile, EducationTile, ProjectsTile } from './componentIndex';
 
 // This is a layout composition component for any components that are bulma tiles.
 export class TileBox extends Component {
@@ -8,6 +8,7 @@ export class TileBox extends Component {
     return (
       <Tile kind="ancestor" vertical>
         <SkillsTile />
+        <ProjectsTile />
         <Tile size={12} kind="parent">
           <ExperienceTile />
           <GoalTile />
