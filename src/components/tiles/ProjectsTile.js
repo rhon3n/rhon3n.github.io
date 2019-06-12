98e1259ea2760c2e4ab6a0f612ab77f1c1755b06
Joel Rhine <baronontherhine@gmail.com>
2019-06-11 22:50:46 -0700

    added tilebody component and data

--- src/components/tiles/ProjectsTile.js
+++ src/components/tiles/ProjectsTile.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { Tile } from 'react-bulma-components/full';
 import { TileTitle } from './tileitems/TileTitle';
+import { TileBody } from './tileitems/TileBody';
 
 export class ProjectsTile extends Component {
   state = {
     title: 'RECENT PROJECTS',
+    subtitle: 'React Todo App',
+    todoUrl: 'https://github.com/rhon3n/react-todo-list',
+    body:
+      'A simple, React application that gets a list of dummy todos from JSONplaceholder. This app allows you to add or remove todos from the list. Since this app is used to demonstrate my ability to control state among components in React without using Redux, it does not have a working backend—any changes made will not persist after a page reload.',
   };
 
   render() {
-    const { title } = this.state;
+    const { title, body, subtitle, todoUrl } = this.state;
     return (
-      <Tile kind="child" size={12}>
-        <TileTitle title={title} />
+      <Tile kind="parent" size={12}>
+        <Tile kind="child" notification>
+          {' '}
+          <TileTitle title={title} />
+          <TileBody url={todoUrl} title={subtitle} body={body} />
+        </Tile>
       </Tile>
     );
   }
