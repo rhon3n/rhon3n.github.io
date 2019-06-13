b78aa79f97991fae2e7c51b80321b0521d886a40
Joel Rhine <baronontherhine@gmail.com>
2019-06-13 10:52:58 -0700

    added and styled icons

--- src/components/tiles/ProjectsTile.js
+++ src/components/tiles/ProjectsTile.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 import { Tile, Box } from 'react-bulma-components/full';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { TileTitle } from './tileitems/TileTitle';
 import { TileBody } from './tileitems/TileBody';
 
 export class ProjectsTile extends Component {
   state = {
     title: 'RECENT PROJECTS',
-    todoTitle: 'React Todo App',
+    todoTitle: 'Todo App',
     todoURL: 'https://github.com/rhon3n/react-todo-list',
     todoBody:
       'A simple react application that gets a list of dummy todos from JSONplaceholder. This app allows you to add or remove todos from the list. Since this app is used to demonstrate my ability to control state among components in React without using Redux, it does not have a working backend—any changes made will not persist after a page reload.',
     cliTitle: 'Command Line Resume',
     cliURL: 'https://www.npmjs.com/package/rhonen-dev',
     cliBody:
-      'Built a command line resume using Node. Run npm i -g rhonen-dev to install, then rhonen-dev to start the program.',
+      'A command line resume built using Node and the chalk.js library for an improved user experience. This application walks you through various aspects of my experience as a developer and person in general.',
   };
 
   render() {
     const { title, todoBody, todoTitle, todoURL, cliBody, cliTitle, cliURL } = this.state;
+    const todoIcon = (
+      <FontAwesomeIcon icon={['fab', 'react']} size="3x" transform="down-3" className="has-text-grey" />
+    );
+    const npmIcon = <FontAwesomeIcon icon={['fab', 'npm']} size="3x" transform="down-3" className="has-text-grey" />;
+
     return (
       <Box className="has-background-light">
         <TileTitle title={title} />
         <Tile kind="parent" size={12}>
           <Tile kind="child" notification>
-            <TileBody url={todoURL} title={todoTitle} body={todoBody} />
+            <TileBody url={todoURL} title={todoTitle} body={todoBody} icon={todoIcon} />
           </Tile>
-          <Tile kind="child" notification> <TileBody url={cliURL} title={cliTitle} body={cliBody} /></Tile>
-         
+          <Tile kind="child" notification>
+            {' '}
+            <TileBody url={cliURL} title={cliTitle} body={cliBody} icon={npmIcon} />
+          </Tile>
         </Tile>
       </Box>
     );
--- src/components/tiles/tileitems/TileBody.js
+++ src/components/tiles/tileitems/TileBody.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // This component returns a string that is passed in as a property.
 // Add the string as a value on the parent component's state object.
 
 export class TileBody extends Component {
   render() {
-    const { body, title, url } = this.props;
+    const { body, title, url, icon } = this.props;
     return (
       <React.Fragment>
         <a className="title has-background-primary is-size-4 has-text-white highlight-title" href={url}>
           {title}
         </a>
+        <br />
+        <span>{icon}</span>
+        <br />
+        <br />
         <p className="is-size-5">{body}</p>
       </React.Fragment>
     );
