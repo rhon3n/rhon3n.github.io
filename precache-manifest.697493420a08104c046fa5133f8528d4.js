f16706749b241c25a75c424f4666cc880b7ae92d
Joel Rhine <baronontherhine@gmail.com>
2019-09-18 18:48:21 -0700

    Updates

---
+++ precache-manifest.697493420a08104c046fa5133f8528d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe444dacea728187295",
+    "url": "/static/css/main.c418d4df.chunk.css"
+  },
+  {
+    "revision": "3fe444dacea728187295",
+    "url": "/static/js/main.cf7ebda0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a0951abb18efdf0beb1",
+    "url": "/static/js/2.e8b167db.chunk.js"
+  },
+  {
+    "revision": "85c3168c56d215656248294f3162d687",
+    "url": "/index.html"
+  }
+];
