f034a62606231dfc9f1c60b01233570070e81b83
rhon3n <baronontherhine@gmail.com>
2020-02-14 13:58:42 -0800

    minor formatting change.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import './dopestyles.css'
+import './dopestyles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
